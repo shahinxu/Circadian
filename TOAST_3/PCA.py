028b4c7dd61eccb5f3a52f7669a8e84684250520
heiroz <1849197801@qq.com>
2025-12-03 15:09:27 -0500

    8

--- TOAST_3/PCA.py
+++
@@ -1,8 +0,0 @@
-from sklearn.decomposition import PCA
-
-def create_eigengenes(expression_scaled, n_components=50):
-    pca = PCA(n_components=n_components)
-    components = pca.fit_transform(expression_scaled)
-    explained_variance = pca.explained_variance_ratio_
-    
-    return components, pca, explained_variance
